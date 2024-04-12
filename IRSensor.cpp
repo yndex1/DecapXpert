f6ece2e040d6a0f090077be6a419215d644292dd
yndex <luca@renold.ch>
2024-04-12 14:53:56 +0200

    ROME2 stuff added

---
+++ IRSensor.cpp
@@ -0,0 +1,51 @@
+/*
+ * IRSensor.h
+ * Copyright (c) 2024, ZHAW
+ * All rights reserved.
+ */
+
+#include <cmath>
+#include "IRSensor.h"
+
+using namespace std;
+
+/**
+ * Creates and initialises the driver to read the distance sensors.
+ * @param distance the analog input to read a distance value from.
+ * @param bit0 a digital output to control the multiplexer.
+ * @param bit1 a digital output to control the multiplexer.
+ * @param bit2 a digital output to control the multiplexer.
+ * @param number the number of the sensor. This value must be between 0 and 5.
+ */
+IRSensor::IRSensor(AnalogIn& distance, DigitalOut& bit0, DigitalOut& bit1, DigitalOut& bit2, int number) : distance(distance), bit0(bit0), bit1(bit1), bit2(bit2) {
+    
+    this->number = number;
+}
+
+/**
+ * Deletes this IRSensor object and releases all allocated resources.
+ */
+IRSensor::~IRSensor() {}
+
+/**
+ * This method reads from the distance sensor.
+ * @return a distance value, given in [m].
+ */
+float IRSensor::read() {
+    
+    bit0 = (number >> 0) & 1;
+    bit1 = (number >> 1) & 1;
+    bit2 = (number >> 2) & 1;
+    
+    float d = 0.09f/(distance+0.001f)-0.03f;  // calculate the distance in [m]
+    
+    return d;
+}
+
+/**
+ * The empty operator is a shorthand notation of the <code>read()</code> method.
+ */
+IRSensor::operator float() {
+    
+    return read();
+}
--- main.cpp
+++ main.cpp
@@ -1,22 +1,54 @@
-/* mbed Microcontroller Library
- * Copyright (c) 2019 ARM Limited
- * SPDX-License-Identifier: Apache-2.0
+/*
+ * Main.cpp
+ * Copyright (c) 2024, ZHAW
+ * All rights reserved.
  */
 
-#include "mbed.h"
+#include <stdio.h>
+#include <mbed.h>
 
-
-// Blinking rate in milliseconds
-#define BLINKING_RATE     500ms
-
-
-int main()
-{
-    // Initialise the digital pin LED1 as an output
+int main() {
+    
+    // create miscellaneous periphery objects
+    
+    DigitalIn button(BUTTON1);
     DigitalOut led(LED1);
-
+    
+    DigitalOut led0(PD_4);
+    DigitalOut led1(PD_3);
+    DigitalOut led2(PD_6);
+    DigitalOut led3(PD_2);
+    DigitalOut led4(PD_7);
+    DigitalOut led5(PD_5);
+    
+    // create LIDAR device driver
+    
+    PwmOut pwm(PE_9);
+    pwm.period(0.00005f);
+    pwm.write(0.5f);
+    
+    ThisThread::sleep_for(500ms);
+    
+    UnbufferedSerial* serial = new UnbufferedSerial(PG_14, PG_9);
+    LIDAR* lidar = new LIDAR(*serial);
+    
+    // create ethernet interface and webserver
+    
+    DigitalOut enableRouter(PB_15);
+    enableRouter = 1;
+    
+    EthernetInterface* ethernet = new EthernetInterface();
+    ethernet->set_network("192.168.0.10", "255.255.255.0", "192.168.0.1"); // configure IP address, netmask and gateway address
+    ethernet->connect();
+    
+    HTTPServer* httpServer = new HTTPServer(*ethernet);
+    httpServer->add("lidar", new HTTPScriptLIDAR(*lidar));
+    
     while (true) {
+        
         led = !led;
-        ThisThread::sleep_for(BLINKING_RATE);
+        
+
+        ThisThread::sleep_for(250ms);
     }
 }
