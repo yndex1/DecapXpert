229cce99aa84d357ae7b7419661adae240b64bb5
yndex1 <161329898+yndex1@users.noreply.github.com>
2024-04-12 11:12:33 +0200

    add blinky

--- main.cpp
+++ main.cpp
@@ -1,5 +1,22 @@
-int main() {
+/* mbed Microcontroller Library
+ * Copyright (c) 2019 ARM Limited
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "mbed.h"
 
 
-  return 0;
+// Blinking rate in milliseconds
+#define BLINKING_RATE     500ms
+
+
+int main()
+{
+    // Initialise the digital pin LED1 as an output
+    DigitalOut led(LED1);
+
+    while (true) {
+        led = !led;
+        ThisThread::sleep_for(BLINKING_RATE);
+    }
 }
